bc41f5657a20afbfd6b6cec117866acf88e736a0
Jose Valentin Abundo Hernandez <valentin.abundo@bambu-mobile.com>
2025-03-19 21:20:15 -0600

    Initial Commit

---
+++ Pokedex_MVVM/Pokedex_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_MVVMApp.swift
+//  Pokedex_MVVM
+//
+//  Created by Developer on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pokedex_MVVMTests/Pokedex_MVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_MVVMTests.swift
+//  Pokedex_MVVMTests
+//
+//  Created by Developer on 19/03/25.
+//
+
+import Testing
+@testable import Pokedex_MVVM
+
+struct Pokedex_MVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
